e4dfd961032def3b601068a8f69ece7ba575ae56
Ashfin Ramadhandy E <ashfinramadhandy10@gmail.com>
2018-11-26 14:40:35 +0700

    menambahkan array list dan model

---
+++ test/Ashfin/barang/Barang.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ashfin.barang;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dhandy
+ */
+public class Barang {
+    private HashMap<String , Float> items = new HashMap<>();
+    //model menampilkan barang dan harga
+    public  Barang(){
+        this.items.put ("Kopi" , new Float (10000));
+        this.items.put ("Susu" , new Float (20000));
+        this.items.put ("Gula" , new Float (30000));
+    }
+    //membuat arraylist
+    public ArrayList<String> getNames(){
+        ArrayList <String> brg = new ArrayList<>();
+        for(String item : this.items.keySet()){
+            brg.add(item);
+        }
+        return brg;
+    }
+    
+    public ArrayList <Float> getPrices(){
+        ArrayList<Float> hrg = new ArrayList<>();
+        for (float item : this.items.values()){
+            hrg.add(item);
+        }
+        return hrg;
+    }
+    
+    public void addItem(String nama, float harga){
+        this.items.put (nama,harga);
+    }
+}
