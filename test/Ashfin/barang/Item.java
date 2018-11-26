99cc5fea19d445797772bfd819ca7e3580aa8b41
Dhandy <Dhandy@DESKTOP-VIG5JU5>
2018-11-26 13:49:11 +0700

    pembuatan class item

---
+++ test/Ashfin/barang/Item.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ashfin.barang;
+
+/**
+ *
+ * @author Dhandy
+ */
+//Deklarasi class item untuk mempresentasikan barang
+//yang akan dimasukan dalam daftar belanja.
+public class Item {
+    //atribut yg digunkana untuk menyimpan nama barang
+    private String name;
+}
